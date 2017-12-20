225e13effe69733d7439dd433f1947f95e4981f5
Valentina Taroi <vtaroi@cisco.com>
2017-12-20 15:26:41 +0200

    form improvments

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
+import {loadCourses} from './actions/courseActions';
+import {loadAuthors} from './actions/authorActions';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
 	<Provider store={store}>
-		<Router history={browserHistory} routes={routes} />
+	<Router history={browserHistory} routes={routes} />
 	</Provider>,
 	document.getElementById('app')
-);
+	);
