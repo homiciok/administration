e260daf625a9766a5bcf7177781cdeea6a7049e0
Valentina Taroi <vtaroi@cisco.com>
2017-12-21 14:21:04 +0200

    finished

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import authorApi from '../api/mockAuthorApi';
-//import { beginAjaxCall } from './ajaxStatusActions';
+import { beginAjaxCall } from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
@@ -8,7 +8,7 @@ export function loadAuthorsSuccess(authors) {
 
 export function loadAuthors() {
   return dispatch => {
-    //dispatch(beginAjaxCall());
+    dispatch(beginAjaxCall());
     return authorApi.getAllAuthors().then(authors => {
       dispatch(loadAuthorsSuccess(authors));
     }).catch(error => {
